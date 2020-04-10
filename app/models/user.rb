4f96d69fdece69bacafccb6f8142c3230a798964
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-10 20:05:10 +0100

    Edited files with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :username
+  validates :username, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
-
-
 RSpec.describe User, type: :model do
 
   subject { described_class.new }
 
-    it "is valid with valid attributes" do
-      subject.email = "new@email.com"
-      subject.password = "anything"
-      subject.created_at = DateTime.now
-      subject.updated_at = DateTime.now
-      subject.username = "countcatula"
-      expect(subject).to be_valid
-    end
+  it "is valid with valid attributes" do
+    subject.email = "new@email.com"
+    subject.password = "anything"
+    subject.created_at = DateTime.now
+    subject.updated_at = DateTime.now
+    subject.username = "countcatula"
+    expect(subject).to be_valid
+  end
 
   it "is not valid without a username" do
     user = User.new(email: "fake@email.com", password: "fakepassword")
