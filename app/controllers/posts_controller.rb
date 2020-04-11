1130f9f27ef1555205957537cf4f690e9c78a0e9
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-11 18:22:20 +0100

    Adding post to database currently fails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :dashboard_path, flash: { success: "Post was created" }
+    else
+      redirect_to :new_post_path, flash: { danger: "New post was not saved" }
+    end
   end
 
   def show
@@ -13,6 +19,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:image)
+    params.require(:post).permit(:image, :image_cache)
   end
 end
