d68365ac36a99eb7352424c3dfcee2c15f0a6652
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-11 10:46:36 +0100

    Added test for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe 'GET /dashboard when not logged in' do
+    it 'responds with 302' do
+      get :index
+      expect(response).to have_http_status(302)
+    end
+  end
 end
