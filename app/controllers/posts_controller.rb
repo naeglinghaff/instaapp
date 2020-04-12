60211e129c445ed319f7723446b73fca57fcd42c
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-12 10:57:26 +0100

    Fixed permissions for uploads

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to :dashboard_path, flash: { success: "Post was created" }
+      redirect_to :dashboard_url, flash: { success: "Post was created" }
     else
       redirect_to :new_post_path, flash: { danger: "New post was not saved" }
     end
