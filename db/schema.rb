3d7bcf13e00403fc8f769a2a16873f388bb491f6
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-13 14:38:54 +0100

    added description field to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_095653) do
+ActiveRecord::Schema.define(version: 2020_04_13_133109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_095653) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
