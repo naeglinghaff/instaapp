6ed8eb2ac0991e757e8626c230960ff2bd769733
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-13 11:38:29 +0100

    profile styling updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :image, :description, :image_cache, :website])
   end
 
 end
