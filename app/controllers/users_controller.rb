78d990fe54a681b770781e56ffd58530cf3cafbf
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-13 10:47:28 +0100

    grid for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    #user feed dashboards
+    #user profile display
     user_id = current_user.id
     @posts = Post.where(user_id: user_id)
   end
 
-  def show
-    #user profile
+  def total_followers
+    0
   end
 
+  def total_following
+    0
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   has_many :posts
+
+  mount_uploader :image, ImageUploader
 end
