b0b4b7aa22136f69a1f95dc6420b4281b02cea9c
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-16 09:37:04 +0100

    Added new div styling for comment form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'GET /profile when not logged in' do
+  describe 'GET redirect when not logged in' do
+    # needs valid user id to get this route
     it 'responds with 302' do
-      get :profile
+      get :profile, :user_id => user.id
       expect(response).to have_http_status(302)
     end
   end
